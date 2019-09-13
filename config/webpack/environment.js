d3221de8ea74db6f5506a0af3b77ee19b9c6c533
kazuyuki <55286708+imk66@users.noreply.github.com>
2019-09-14 01:47:18 +0900

    add/ckeditor5

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,23 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const ckeditorSVG = require('./loaders/ckeditor-svg')
+const ckeditorCSS = require('./loaders/ckeditor-css')
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' )
+
+environment.plugins.prepend('CKEditor',new CKEditorWebpackPlugin({
+  language: 'en'
+  })
+)
+
+environment.loaders.append('ckeditorCSS', ckeditorCSS)
+environment.loaders.append('ckeditorSVG', ckeditorSVG)
+
+const cssLoader = environment.loaders.get('css');
+cssLoader.exclude = /(\.module\.[a-z]+$)|(ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css)/
+
+const fileLoader = environment.loaders.get('file');
+fileLoader.exclude = /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
